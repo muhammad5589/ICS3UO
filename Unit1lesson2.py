f283830883b2997ac1d5afe357cc3ec1f43629ef
muhammad5589 <752451@pdsb.net>
2025-02-06 16:40:28 -0500

    Unit1lesson2.py

---
+++ Unit1lesson2.py
@@ -0,0 +1,29 @@
+# Predict:
+# I would expect that print(24) would output 24
+# Run:
+# My prediction was correct and it output 24
+# Modify:
+amount = 24
+print("Two dozen is: ", amount)
+# Using the variable amount can help with readability of the code and printing "Two dozen is: " helps give more feedback and information when running it
+# Modify 2:
+amount = 7 * 4
+print("forty-two is: ", amount)
+# The fixed output would be
+amount = 7 * 4
+print("twenty-eight is: ", amount)
+# or
+amount = 7 * 6
+print("forty-two is: ", amount)
+# Modify 3:
+amount = 8
+amount += 1
+print("The amount is: ", amount)
+# amount = amount + 1 is taking the value of the amount variable and adding one to it so 8 + 1 = 9
+# amount += 1 works the same way and adds 1 to the value of amount
+# Modify 4:
+amount = 8
+amount *= 2
+print(amount)
+# amount = amount * 2 is taking the value of amount and multiplying it by two, in this case doubling the value
+# amount *= 2 works the same way and multiplies the amount value by 2
