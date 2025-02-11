8b67c5982976dd7b6202916e6e5f652d556d2602
muhammad5589 <752451@pdsb.net>
2025-02-11 14:24:31 -0500

    Update Unit1lesson7.py

--- Lessons/Unit1lesson7.py
+++ Lessons/Unit1lesson7.py
@@ -1,1 +1,18 @@
+# Predict
+# A) I preditct that the statement would say ("Yes!", y, "is a factor of", x)
+# B) 
 
+
+# Modify
+import math
+
+x = input("Please input a whole number: ")
+x = int(x)
+y = input("Please input another nonzero whole number")
+y = int(y)
+print("Now deciding if", y, "is a factor of", x, "...")
+rem = x % y
+if rem == 0:
+    print("Yes!", y, "is a factor of", x)
+else:
+  print("No!", y, "is not a factor of", x)
