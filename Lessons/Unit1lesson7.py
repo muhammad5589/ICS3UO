d9c8c7f92528b2fea57172afc60a99b56bb9c774
muhammad5589 <752451@pdsb.net>
2025-02-12 20:58:17 -0500

    Update Unit1lesson7.py

--- Lessons/Unit1lesson7.py
+++ Lessons/Unit1lesson7.py
@@ -18,6 +18,8 @@ if rem == 0:
     #Modify 2 
 import math
 #modify
+import math
+
 x = input("Please input a whole number: ")
 x = int(x)
 y = input("Please input another nonzero whole number")
@@ -28,10 +30,11 @@ if rem == 0:
     print("Yes!", y, "is a factor of", x)
 else: 
   print("you can't enter zero. enter a non zero number")
-    
-  #Modify3
-x=input("only accept numbers between 1 and 20:")
+x=input("enter a number between 1 and 20:")
+x=int(x)#converting "x" to an integer  
 if (x < 1) and (x > 20):
   print("Error: the number must be between 1 and 20.")
-else:
-y=input("only accept numbers between 1 and 20:")
+else:y=input("enter number between 1 and 20:")
+y=int(y)#converting "y" to an integer
+if (y < 1) and (y > 20):
+  print("Error: the number must be between 1 and 20.")
