f45d70f9dec293f85d267571f747a99768813bbe
muhammad5589 <752451@pdsb.net>
2025-02-12 21:57:07 -0500

    Update Unit1lesson7.py

--- Lessons/Unit1lesson7.py
+++ Lessons/Unit1lesson7.py
@@ -1,40 +1,27 @@
 # Predict
 # A) I preditct that the statement would say ("Yes!", y, "is a factor of", x)
-# B) 
+# B) I predict that the statement would say ("No", y, "is not a factor of", x)
 
 
-# Modify
 import math
 
 x = input("Please input a whole number: ")
 x = int(x)
+if (x < 1) or (x > 20):
+  print("Error: the number must be between 1 and 20.")
+  exit()
 y = input("Please input another nonzero whole number")
 y = int(y)
-print("Now deciding if", y, "is a factor of", x, "...")
-rem = x % y
-if rem == 0:
-    print("Yes!", y, "is a factor of", x)
-    
-    #Modify 2 
-import math
-#modify
-import math
-
-x = input("Please input a whole number: ")
-x = int(x)
-y = input("Please input another nonzero whole number")
-y = int(y)
+if y==0:
+  print("y cant be 0")
+  exit()
+if (y < 1) or (y > 20):
+  print("Error: the number must be between 1 and 20.")
+  exit()
 print("Now deciding if", y, "is a factor of", x, "...")
 rem = x % y
 if rem == 0:
     print("Yes!", y, "is a factor of", x)
 else: 
-  print("you can't enter zero. enter a non zero number")
-x=input("enter a number between 1 and 20:")
-x=int(x)#converting "x" to an integer  
-if (x < 1) and (x > 20):
-  print("Error: the number must be between 1 and 20.")
-else:y=input("enter number between 1 and 20:")
-y=int(y)#converting "y" to an integer
-if (y < 1) and (y > 20):
-  print("Error: the number must be between 1 and 20.")
+  print("no", y,"is not a factor of", x )
+exit()
