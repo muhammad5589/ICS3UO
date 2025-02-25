8c917d2455353fef3052de92128c28e07713b7fe
muhammad5589 <752451@pdsb.net>
2025-02-25 13:58:51 -0500

    Create Unit1Assignment1.py

---
+++ Lessons/Unit1Assignment1.py
@@ -0,0 +1,7 @@
+print("Welcome to the even and odd detector!") 
+x = input("Enter your first number")
+y = input("Enter your second number") 
+if (x % 2==0) and (y % 2==0):
+  print("the product of both numbers are even")
+elif (x % 2==1) and (y % 2==1):
+  print("the bproduct of both numbers is odd")
