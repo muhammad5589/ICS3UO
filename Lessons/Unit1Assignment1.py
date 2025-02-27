32a8846fd3f74444c87f75fa849c669ffaf9b94d
muhammad5589 <752451@pdsb.net>
2025-02-27 18:20:27 -0500

    Update Unit1Assignment1.py

--- Lessons/Unit1Assignment1.py
+++ Lessons/Unit1Assignment1.py
@@ -14,3 +14,37 @@ j= int(input("Please enter the edge length of your cube:"))
 s=math.sqrt(3)*j
 d= j*s
 print("your inner diagonal is: %.2f" % d)
+
+- 3
+change=int(input("Please enter the amount of change in cents:"))
+if change >= 100:
+  change= change % 100
+
+qamnt= change//25
+qrem= change % 25 
+  
+damnt= qrem//10
+drem= qrem % 10
+  
+namnt= drem//5
+nrem= drem % 5
+  
+pamnt=nrem
+  
+if qamnt==0:
+  print("You have a dollar")
+  
+else:
+  print("cents are:")
+  
+if qamnt>0:
+  print(qamnt,"quarters")
+
+if damnt>0:
+  print(damnt,"dimes")
+  
+if namnt>0:
+  print(namnt,"nickles")
+  
+if pamnt>0:
+  print(pamnt, "pennies") 
