ba6c691b9139ac19507092928c4f385d06dafa66
muhammad5589 <752451@pdsb.net>
2025-02-27 13:48:47 -0500

    Update Unit1Assignment1.py

--- Lessons/Unit1Assignment1.py
+++ Lessons/Unit1Assignment1.py
@@ -7,6 +7,9 @@ elif (x % 2==1) and (y % 2==1):
   print("the bproduct of both numbers is odd")
 
 - 2
+import math
+
 print ("I will find the cube's inner diagonal for any edge length!")
 j= int(input("Please enter the edge length of your cube:"))
-s= j*math.sqrt(3)
+s=math.sqrt(3)*j
+print(s)
