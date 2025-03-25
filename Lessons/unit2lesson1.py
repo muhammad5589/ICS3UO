d3c7823198c612a7a9379bd809af1601b03fc03d
muhammad5589 <752451@pdsb.net>
2025-03-25 13:56:29 -0400

    Update unit2lesson1.py

--- Lessons/unit2lesson1.py
+++ Lessons/unit2lesson1.py
@@ -1,1 +1,12 @@
+# I predict that if I choose one of the three, I will get the correct output
+Make a choice from the following menu: 
+A: apples
+B: bananas
+C: cherries
+Your choice:  C
+I prefer cherries
+
+#predict
+I predict that phython would crash and I would not get an output
+
 
